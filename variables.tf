001e235aae741e4fd224b0031327b859db73d69d
Spartan - Y Truong <y.truong@c0x12c.com>
2025-10-03 14:40:34 +0700

    feat: introduced new sub-field resources in assume_custom_roles (#3)

--- variables.tf
+++ variables.tf
@@ -67,11 +67,12 @@ variable "keda_role_arn" {
 variable "assume_custom_roles" {
   description = "List of custom IAM assume role statements."
   type = list(object({
-    sid         = string
+    sid         = optional(string, null)
     effect      = string
     actions     = list(string)
-    type        = string
-    identifiers = list(string)
+    type        = optional(string, null)
+    identifiers = optional(list(string), null)
+    resources   = optional(list(string), null)
     condition = optional(object({
       test     = string
       variable = string
